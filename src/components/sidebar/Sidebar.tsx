b09431a28684c0337d11a93ae7970a7009f48332
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-16 18:01:08 +0900

    Add: サイドバーのフッターを作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
+import SettingsIcon from "@mui/icons-material/Settings";
 import SidebarChannel from "./SidebarChannel";
 import "./Sidebar.scss";
 
@@ -36,6 +39,22 @@ const Sidebar = () => {
             <SidebarChannel />
             <SidebarChannel />
             <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" />
+              <div className="accountName">
+                <h4>ユーザー名</h4>
+                <span>#0000</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadsetMicIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
