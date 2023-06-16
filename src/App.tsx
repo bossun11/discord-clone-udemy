f6da90ff44587eb537e07b9e82aea51b1328f2d2
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-16 15:51:44 +0900

    Add: Sidebarコンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
+import "./components/sidebar/Sidebar.scss";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return <div className="App">discord</div>;
+  return (
+    <div className="App">
+      {/* sidebar */}
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
