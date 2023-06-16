0b4a922053b3b677c1574544f4c2dca81d015273
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-16 15:05:42 +0900

    Add: Sassをインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">discord</div>;
 }
 
 export default App;
