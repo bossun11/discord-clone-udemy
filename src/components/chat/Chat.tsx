74237b4274717e4a9c1beacd41cc36305bcd3f65
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-17 13:20:01 +0900

    Add: チャットヘッダーコンポーネントを作成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Chat.scss";
+import ChatHeader from "./ChatHeader";
 
 const Chat = () => {
   return (
     <div className="chat">
       {/* chatHeader */}
+      <ChatHeader />
       {/* chatMessage */}
+      <div className="chatMessages"></div>
       {/* chatInput */}
+      <div className="chatInput"></div>
     </div>
   );
 };
