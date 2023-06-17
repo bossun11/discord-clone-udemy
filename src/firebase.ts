02ba1b905123bbcd4a46d5caf3e695d2789f2fdc
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-17 16:17:35 +0900

    Add: Firebaseの初期設定を実施

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ6Yzzj_xTiO9SM1KG22i9GrhsYCZqRnU",
+  authDomain: "discord-clone-udemy-723ba.firebaseapp.com",
+  projectId: "discord-clone-udemy-723ba",
+  storageBucket: "discord-clone-udemy-723ba.appspot.com",
+  messagingSenderId: "591779283753",
+  appId: "1:591779283753:web:94f309245b6932721f2d38",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
